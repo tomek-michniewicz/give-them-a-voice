609b5b9971af126944a283038b2470125092362a
Tomek Michniewicz <michniewicz.tomasz@gmail.com>
2023-03-04 17:02:08 -0800

    feat: added two frontend projects -> give-them-a-voice-user and give-them-a-voice-hub

---
+++ give-them-a-voice-user/src/App.tsx
@@ -0,0 +1,17 @@
+import { Button, Container, Heading, Text } from '@chakra-ui/react'
+import './App.css'
+
+function App() {
+
+  return (
+    <Container>
+      <Heading>Hello!</Heading>
+      <Text my={4}>
+        give-them-a-voice-user
+      </Text>
+      <Button colorScheme='blue'>Ok</Button>
+    </Container>
+  )
+}
+
+export default App
